277e1811720afc2ebbb207bbc1d06bb7e29af5cf
ne-supanat <naesup@gmail.com>
2025-09-25 14:30:34 +0700

    - added github action auto web release - updated app title

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Gradient Slide Demo',
+        title: 'Iridescent Slide Puzzle',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
